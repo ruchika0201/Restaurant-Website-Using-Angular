21e43acbec309b6884561b25371741554787eac4
Ruchika Pande <ruchikapande99@gmail.com>
2020-05-17 20:47:24 +0530

    Assignment 4 Task 1 and 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,26 +3,38 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import {Observable, of} from 'rxjs'
 import {delay } from 'rxjs/operators'
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService) { }
   getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership')
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+    //return of(LEADERS).pipe(delay(2000));
     
   }
 
 
   getLeader(id: number):   Observable<Leader> {
-    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+    return this.http.get<Leader>(baseURL + 'leadership/' + id)
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+    //return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
     
     //return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
   }
 
   getFeaturedLeader():  Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    return this.http.get<Leader[]>(baseURL + 'leadership?featured=true').pipe(map(leader => leader[0]))
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+    //return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
     
    // return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0];
   }
